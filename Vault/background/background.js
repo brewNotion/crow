b750374fa507c560aaf039af295377cb969f1cf0
Brew Notion <brewnotion@gmail.com>
2018-06-02 22:36:34 +0530

    4mWin displays all folders in popup, adds new folder

--- Vault/background/background.js
+++ Vault/background/background.js
@@ -1,13 +1,37 @@
 
+function Folder(id,name){
+    this.id = id
+    this.name = name;
+    this.files = {};
+    // this.subFolders = {};
+  }
+  
+  function File(id,desc,link){
+    this.id = id
+    this.description = desc
+    this.link = link
+  }
 
 chrome.runtime.onInstalled.addListener(function() {
-    console.log("what is happening")
+    chrome.storage.sync.clear(function(){
+        var error = chrome.runtime.lastError
+        if(error)
+            console.log("Error while clearing Storage")
+        else
+            console.log("Storage successfully cleared")
+    })
+    console.log("Background loaded")
+    chrome.storage.sync.set({'0':(new Folder(0,"root"))},function(){
+        console.log("root folder set")
+    })
     chrome.storage.sync.get('folderId', function(data){
-        if(data.folderId == undefined){
-            chrome.storage.sync.set({'folderId':data.folderId+1},function(){
-                console.log('Added Folder Id :'+(data.folderId+1))
-            });
-        }else 
+        console.log("getting folderId")
+        if(data.folderId === undefined){
+            chrome.storage.sync.set({'folderId':0},function(){
+                console.log('Initialized last folder Id :'+0)
+            })
+        }else{
             console.log("last folder id :"+data.folderId)
+        }
     });
 });
--- Vault/popup/popup.js
+++ Vault/popup/popup.js
@@ -1,20 +1,54 @@
 
+function Folder(id,name){
+    this.id = id
+    this.name = name;
+    this.files = {};
+    this.subFolders = [];
+  }
+  
+  function File(id,desc,link){
+    this.id = id
+    this.description = desc
+    this.link = link
+  }
 
 let bAddFolder = document.getElementById('b-add-folder')
 let folderList = document.getElementById('folder-list')
 
+var appendFolder = function(id){
+    id = id.toString()
+    chrome.storage.sync.get(id, function(data){
+        var folder = data[id]
+        var newLi = document.createElement('li');
+        newLi.innerHTML = "<input type='checkbox' value='"+id+"'><span>"+folder.name+"</span>"
+        folderList.appendChild(newLi)
+    })
+}
+
+document.getElementById("popup-body").onload = function(){
+    chrome.storage.sync.get('folderId',function(data){
+        var folderId = parseInt(data.folderId)
+        
+        for(i=0; i<=folderId; i++){
+            appendFolder(i)
+        }
+    });
+}
+
 bAddFolder.onclick = function(element){
     var folderName = document.getElementById("i-folder-name").value
     chrome.storage.sync.get('folderId',function(data){
-        fId = data.folderId + 1
-        chrome.storage.sync.set({fId:folderName},function(){
-            alert('Added fodler:'+fId+'->'+folderName)
+        var fId = (parseInt(data.folderId)+1).toString()
+        //for sending a message
+        var folderJson =  {}
+        folderJson[fId] = (new Folder(fId,folderName))
+        chrome.storage.sync.set(folderJson,function(error){
+            
+            appendFolder(fId)
+
+            chrome.storage.sync.set({'folderId':fId}, function(){
+                console.log('folder Id updated to '+fId)
+            })
         })
-        chrome.storage.sync.set({'folderId':fId}, function(){
-            console.log('folder Id updated to '+fId)
-        })
-        var newFolder = document.createElement('li');
-        newFolder.innerHTML = "<checkbox value="+fId+"><span>"+folderName+"<span>"
-        folderList.appendChild(newFolder)
     });
 }
